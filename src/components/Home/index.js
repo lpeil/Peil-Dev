a27c02dccf164631a3f8ab9aef05440847dfbff6
Luan Peil <luan@peil.dev>
2019-10-30 15:15:23 -0300

    Add Efects in Home and NavBar

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { TimelineLite } from "gsap/all";
+
 
 import HomeBackground from '../../assets/home-background.jpg';
 
@@ -10,7 +12,7 @@ const HomePage = styled.div`
     align-items: center;
     height: 100vh;
     width: 100%;
-    margin-top: -50px;
+    margin-top: -75px;
     background-image: url(${HomeBackground});
     background-position: center center;
     background-size: cover;
@@ -27,11 +29,13 @@ const HomeInfos = styled.div`
 `
 
 const Name = styled.span`
+    position: relative;
     font-size: 75px;
     font-family: 'Roboto-Slab', serif;
 `
 
 const Description = styled.span`
+    position: relative;
     font-size: 33px;
 `
 
@@ -40,16 +44,35 @@ const Space = styled.div`
     width: 100%;
 `
 
-const Home = () => (
-    <>
-        <HomePage>
-            <HomeInfos>
-                <Name>Luan Peil</Name>
-                <Description>Desenvolvedor Web</Description>
-            </HomeInfos>
-        </HomePage>
-        <Space></Space>
-    </>
-)
+export default class Home extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.textEnter = new TimelineLite({ paused:false });
 
-export default Home;
+		this.content = null;
+		this.name = null;
+		this.desc = null;
+	}
+
+
+	componentDidMount(){
+		this.textEnter
+            .set(this.content, { autoAlpha: 1 })
+			.from(this.name, 0.5, { left: 200, autoAlpha: 0 })
+			.from(this.desc, 1, { left: -200, autoAlpha: 0 }, "-=0.25")
+	}
+    
+    render() {
+        return(
+            <>
+                <HomePage>
+                    <HomeInfos ref={ div => this.content = div }>
+                        <Name ref={ span => this.name = span }>Luan Peil</Name>
+                        <Description ref={ span => this.desc = span}>Desenvolvedor Web</Description>
+                    </HomeInfos>
+                </HomePage>
+                <Space></Space>
+            </> 
+    )}
+}
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -9,20 +9,22 @@ const Wrapper = styled.div`
     justify-content: space-around;
     align-items: center;
     width: 100%;
-    height: 50px;
+    height: 75px;
     background-color: transparent;
     z-index: 1;
+    transition: 0.4s;
 `
 
 const FakeSpace = styled.div`
     position: relative;
     width: 100%;
-    height: 50px;
+    height: 75px;
     z-index: -1;
 `
 
 const Logo = styled.div`
     font-size: 24px;
+    color: #000;
 `
 
 const LinksWrapper = styled.div`
@@ -31,23 +33,66 @@ const LinksWrapper = styled.div`
     justify-content: center;
 `
 
-const NavLink = styled(Link)`
+const MenuLink = styled(NavLink)`
+    position:relative;
     margin: 0 10px;
+    color: #000;
+    font-size: 18px;
+    border: 0;
+    border-bottom: 2px solid transparent;
+    transition: color 0.25s;
+    transition: all 0.5s;
+
+    &:after {
+        content:" ";
+        width: 0px;
+        position: absolute;
+        transition: all 0.5s;
+        bottom: -1px;
+        left: -1px;
+        border-bottom: 2px solid #000;
+    }
+
+    &:hover:after {
+        width: 100%;
+    }
+
+    &.active:after {
+        width: 100%;
+    }
 `
 
-const Navbar = () => (
-    <>
-        <Wrapper>
-            <Logo>Luan Peil</Logo>
-            <LinksWrapper>
-                <NavLink to="/">ÍNICIO</NavLink>
-                <NavLink to="/projetos">PROJETOS</NavLink>
-                <NavLink to="/sobre">SOBRE</NavLink>
-                <NavLink to="/contato">CONTATO</NavLink>
-            </LinksWrapper>
-        </Wrapper>
-        <FakeSpace></FakeSpace>
-    </>
-)
-
-export default Navbar;
+export default class Navbar extends React.Component {
+    state = {
+        color: 'white'
+    }
+    
+    listenScrollEvent = e => {
+        if (window.scrollY > 100) {
+            this.setState({backgroundColor: '#4D6C80'})
+        } else {
+            this.setState({backgroundColor: 'transparent'})
+        }
+    }
+    
+    componentDidMount() {
+        window.addEventListener('scroll', this.listenScrollEvent)
+    }
+    
+    render() {
+        return(
+            <>
+                <Wrapper style={{color: this.state.color, backgroundColor: this.state.backgroundColor}}>
+                    <Logo>Luan Peil</Logo>
+                    <LinksWrapper>
+                        <MenuLink to="/" exact activeClassName="active">ÍNICIO</MenuLink>
+                        <MenuLink to="/projetos" activeClassName="active">PROJETOS</MenuLink>
+                        <MenuLink to="/sobre" activeClassName="active">SOBRE</MenuLink>
+                        <MenuLink to="/contato" activeClassName="active">CONTATO</MenuLink>
+                    </LinksWrapper>
+                </Wrapper>
+                <FakeSpace></FakeSpace>
+            </>
+        )
+    }
+}
